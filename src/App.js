f55b38aada10aec24a18eebd57daadd8082d9f6b
codeForever033 <chatt.abhi033@gmail.com>
2021-05-09 00:34:19 +0530

    commit -m alert state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import './App.css'; //global css
 import axios from 'axios';
 
@@ -10,7 +11,8 @@ class App extends Component{
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
   // async componentDidMount(){
 
@@ -40,6 +42,14 @@ class App extends Component{
   //clear users from state
   clearUsers = () => this.setState( { users: [], loading: false } );
 
+  // set alert
+  setAlert = (msg, type) => {
+    // this.setState({ alert: {msg: msg, type:type } });
+    this.setState({ alert: {msg, type } });
+
+    setTimeout( ( ) => this.setState( { alert: null } ), 2000 );
+  }
+
   render (){
      
     const {users, loading} = this.state;
@@ -47,11 +57,13 @@ class App extends Component{
       <div className='App'>
         <Navbar title="Github Finder" icon = 'fa fa-github'/>
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
             showClear={
             users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
