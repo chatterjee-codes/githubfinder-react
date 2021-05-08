3b14abbb7c15fd60386e35859dece09f963390a6
codeForever033 <chatt.abhi033@gmail.com>
2021-05-09 01:58:20 +0530

    commit -m routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import './App.css'; //global css
 import axios from 'axios';
+
 
 class App extends Component{
 
@@ -51,24 +54,37 @@ class App extends Component{
   }
 
   render (){
-     
+    
+
+
     const {users, loading} = this.state;
     return (
-      <div className='App'>
-        <Navbar title="Github Finder" icon = 'fa fa-github'/>
-        <div className="container">
+      <Router>
+        <div className='App'>
+          <Navbar title="Github Finder" icon = 'fa fa-github'/>
+          <div className="container">
           <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={
-            users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={
+                    users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+              </Fragment>
+              )} 
+            />
+            <Route exact path='/about' component={About}/>
+          </Switch>
+            
+          </div>
+          
         </div>
-        
-      </div>
+      </Router>
     );
 
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from React')); without jsx
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,6 +12,15 @@ const Navbar = ({ icon, title })=> {
                     {/* props getting pulled from app.js */}
                     <i className={icon}> {title} </i> 
                 </h1>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
             </nav>
         )
     
